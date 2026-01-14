45bdb4d1b3b6a5422eaf03c1ae6b075a47e07c3f
Orlando Pedrazzoli <pedrazzoliorlando@gmail.com>
2026-01-14 16:39:57 +0000

    vete config updated

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -1,9 +1,24 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
+import path from 'path';
 
 export default defineConfig({
   plugins: [react()],
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, './src'),
+    },
+  },
   build: {
-    chunkSizeWarningLimit: 2000 // aumenta o limite do Vite para evitar warnings
-  }
+    outDir: 'dist',
+    chunkSizeWarningLimit: 2000,
+    rollupOptions: {
+      output: {
+        manualChunks: {
+          vendor: ['react', 'react-dom', 'react-router-dom'],
+          query: ['@tanstack/react-query'],
+        },
+      },
+    },
+  },
 });
